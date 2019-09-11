8b5b20923c5122c9bb23dabbc1bf45c04c2edd2e
Jake Writer <jakewriter@Jakes-MacBook-Pro.local>
2019-09-11 10:08:05 -0600

    Version with Accessors and constructors git push

---
+++ src/Node.java
@@ -0,0 +1,41 @@
+ public class Node<T>{
+        private T data;
+        private Node front;
+        private Node back;
+
+        public Node(){
+
+        }
+        public Node(T datain) {
+            data = datain;
+
+        }
+        public Node(T datain, Node front, Node back){
+            data = datain;
+            this.front = front;
+            this.back = back;
+        }
+        public Node getFront(){
+            return front;
+        }
+        public Node getBack(){
+            return back;
+        }
+        public T getData(){
+            return data;
+        }
+        @Override
+        public String toString() {
+            return "Node: " + data.toString();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if(o instanceof Node){
+                return this.getData().equals(((Node)o).getData());
+            }else{
+                return false;
+            }
+        }
+    }
+
