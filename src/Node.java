f35b6ad47ea57cd4f44869ceb61436c48dd8a7bf
Jake Writer <jakewriter@Jakes-MacBook-Pro.local>
2019-09-19 15:31:43 -0600

    fixed FIXMEs

--- src/Node.java
+++ src/Node.java
@@ -11,38 +11,53 @@ public class Node<T>{
         private Node back;
         //FIXME: Add comments with what the constructor/method takes it, and a proper name
 
+        //empty constructor all values are null
         public Node(){
 
         }
-        public Node(T datain) {
+        //constructor that sets data to the inputted value, rest of values are null
+        public Node(T datain){
             data = datain;
 
         }
+        //constructor takes in and sets the data and the front & back Nodes
         public Node(T datain, Node front, Node back){
             data = datain;
             this.front = front;
             this.back = back;
         }
+        //returns front value
         public Node getFront(){
             return front;
         }
+        //returns back value
         public Node getBack(){
             return back;
         }
+        //returns data
         public T getData(){
             return data;
         }
-        //FIXME: fix indentation and whitespace
-        public void setData(T newData){ data = newData;}
-     public void setFront(Node newFront){ front = newFront;}
-     public void setBack(Node newBack){ back = newBack;}
+        //sets data
+        public void setData(T newData){
+            data = newData;
+        }
+        //sets new front
+     public void setFront(Node newFront){
+            front = newFront;
+        }
+        //sets new back
+     public void setBack(Node newBack){
+            back = newBack;
+        }
+        //toString override for better formatting
         @Override
-        public String toString() {
+        public String toString(){
             return "Node: " + data.toString();
         }
-
+        //.equals method that can take in any Object but will return false if not a Node of the same value
         @Override
-        public boolean equals(Object o) {
+        public boolean equals(Object o){
             if(o instanceof Node){
                 return this.getData().equals(((Node)o).getData());
             }else{
--- src/NodeDriver.java
+++ src/NodeDriver.java
@@ -9,20 +9,19 @@ public class NodeDriver {
         Node a = new Node(4);
         Node b = new Node(8);
         Node n = new Node(5,a, b);
-        System.out.println("back of n: "+ n.getBack());
-        System.out.println("front of n: " + n.getFront());
-        System.out.println("data from n: " + n.getData());
+        System.out.println("back of n Expected value: " + "Node: 8"+ " Actual value: " + n.getBack());
+        System.out.println("front of n Expected value: " + "Node: 4"+ " Actual value: " + n.getFront());
+        System.out.println("data from n Expected value: " +"5" + " Actual value: " + n.getData());
         n.setBack(a);
         n.setFront(b);
         n.setData(10);
-        System.out.println(a.toString());
-        System.out.println("a is equal to b: " + a.equals(b));
-        System.out.println("new back of n: "+ n.getBack());
-        System.out.println("new front of n: " + n.getFront());
-        System.out.println("new data from n: " + n.getData());
+        System.out.println("Expected toString: Node: 4" + " actual toString: " + a.toString());
+        System.out.println("a is equal to b Expected value: false, Actual value: " + a.equals(b));
+        System.out.println("new back of n Expected value: Node: 4, Actual value: "+ n.getBack());
+        System.out.println("new front of n Expected value: Node: 8, Actual value: " + n.getFront());
+        System.out.println("new data from n Expected value: 10, Actual value: " + n.getData());
         a.setData(8);
-        System.out.println("a is equal to b: " + a.equals(b));
-
+        System.out.println("a is equal to b: Expected value: true, Actual value: " + a.equals(b));
 
     }
 }
