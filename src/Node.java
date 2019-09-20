3197119a1a413bb0fa4e83d05e9904342b4b9d3a
Jake Writer <jakewriter@Jakes-MacBook-Pro.local>
2019-09-20 11:47:44 -0600

    added spacing

--- src/Node.java
+++ src/Node.java
@@ -14,46 +14,56 @@ public class Node<T>{
         public Node(){
 
         }
+
         //constructor that sets data to the inputted value, rest of values are null
         public Node(T datain){
             data = datain;
 
         }
+
         //constructor takes in and sets the data and the front & back Nodes
         public Node(T datain, Node front, Node back){
             data = datain;
             this.front = front;
             this.back = back;
         }
+
         //returns front value
         public Node getFront(){
             return front;
         }
+
         //returns back value
         public Node getBack(){
             return back;
         }
+
         //returns data
         public T getData(){
             return data;
         }
+
         //sets data
         public void setData(T newData){
             data = newData;
         }
+
         //sets new front
      public void setFront(Node newFront){
             front = newFront;
         }
+
         //sets new back
      public void setBack(Node newBack){
             back = newBack;
         }
+
         //toString override for better formatting
         @Override
         public String toString(){
             return "Node: " + data.toString();
         }
+        
         //.equals method that can take in any Object but will return false if not a Node of the same value
         @Override
         public boolean equals(Object o){
